358d821d2dcf870297961c652ad4a5c4e6c9b066
taquy <quyt@unifiedpost.com>
2021-09-10 01:01:57 +0700

    update gluster peering script

--- gluster/peering.sh
+++ gluster/peering.sh
@@ -1,9 +1,3 @@
 INTERNAL_IPS=$(kubectl get nodes -o jsonpath={.items[*].status.addresses[?\(@.type==\"InternalIP\"\)].address})
 for i in $INTERNAL_IPS; do gluster peer probe $i; done
 gluster peer status
-
-BRICKS=""
-VOLUME_NAME="gv0"
-mkdir -p /data/$VOLUME_NAME
-for i in $INTERNAL_IPS; do BRICKS+=" ${i}:/data/${VOLUME_NAME} "; done
-gluster volume create $VOLUME_NAME replica 2 $BRICKS force
